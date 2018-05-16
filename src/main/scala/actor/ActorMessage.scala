5d3d327a52b40d06fba0841781793f7ff1acb6cf
liyubin <lixin58688@163.com>
2018-05-16 19:14:05 +0800

    完善Actor Message

--- src/main/scala/actor/ActorMessage.scala
+++ src/main/scala/actor/ActorMessage.scala
@@ -1,10 +1,13 @@
 package actor
 
-import scala.actors.Actor
+import scala.actors.{Future, Actor}
 
 /**
  * Created by Administrator on 2018/4/27 0027.
  * 用Actor实现同步/异步消息的发送、接收
+ * ! 异步且不要返回值
+ * !! 异步且需要返回值
+ * !? 同步且等待返回值
  */
 class ActorMessage extends Actor{
   override def act(): Unit = {
@@ -14,12 +17,12 @@ class ActorMessage extends Actor{
         case "start" => println("starting...")
         case AsynMsg(id, msg)=>{
           println(s"id:$id, msg:$msg")
-          Thread.sleep(2000)
+          Thread.sleep(1000)
           sender ! ReplyMsg(5, "asyn success")
         }
         case SyncMsg(id, msg)=>{
           println(s"id:$id, msg:$msg")
-          Thread.sleep(2000)
+          Thread.sleep(1000)
           sender ! ReplyMsg(5, "sync success")
         }
       }
@@ -37,5 +40,28 @@ case class ReplyMsg(id:Int, msg:String)
 object ActorMessage extends App{
   val act = new ActorMessage
   act.start()
+  println("发送异步消息前")
+  val a = act ! AsynMsg(1,"liyubin")
+//  if(a.isSet)   //可以看出 ! 后不会有返回值，也就不会有isSet、apply方法
+//    println(a.apply())
+  println("发送异步消息后")
+
+  Thread.sleep(3000)
+  println("---等待3秒---")
+
+  println("发送异步消息且需要返回值前")
+  val future: Future[Any] = act !! AsynMsg(2,"li")
+  Thread.sleep(2000)
+  //获取异步返回值
+  if(future.isSet)
+    println(future.apply())
+  println("发送异步消息且需要返回值后")
+
+  Thread.sleep(3000)
+  println("---等待3秒---")
+
+  println("发送同步消息前")
+  act !? SyncMsg(3,"first")
+  println("发送同步消息后")
 
 }
