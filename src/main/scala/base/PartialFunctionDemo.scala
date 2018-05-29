b61c1462798c4ea397749bf60f1a38429d5aa01a
code159 <409901403@qq.com>
2018-05-29 23:45:28 +0800

    新增关于偏函数的理解

--- src/main/scala/base/PartialFunctionDemo.scala
+++ src/main/scala/base/PartialFunctionDemo.scala
@@ -1,8 +1,22 @@
 package base
 
 //偏函数: 常用于输入模式匹配
+//是PartialFunction[A,B]的实例，A是参数类型，B是返回类型
 object PartialFunctionDemo extends App{
+  def m3(pf:PartialFunction[String,Int]):Int={
+    val result = pf.apply("one")
+    result
+  }
+
+
   def m1: PartialFunction[String, Int] = {
+    case "one" => println("case 1"); 1
+    case "two" => println("case 2"); 2
+    case _ => println("case other"); -1
+  }
+
+  //m3即一个参数为偏函数的方法
+   m3 {
     case "one" => println("case 1"); 1
     case "two" => println("case 2"); 2
     case _ => println("case other"); -1
