a1abe989e4cef99e6485f3845819ad20d44370ef
liyubin <lixin58688@163.com>
2018-08-27 19:13:38 +0800

    新增SparkStreaming从flume pull数据

--- src/main/scala/sparkstreaming/FlumePullWC.scala
+++ src/main/scala/sparkstreaming/FlumePullWC.scala
@@ -1,17 +1,17 @@
 package sparkstreaming
 
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.flume._
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
 
 /**
  * Created by lixin on 2018/8/26.
  */
-object FlumePushWC{
+object FlumePullWC{
   def main(args:Array[String]): Unit ={
     val conf = new SparkConf().setMaster("local[2]").setAppName("FlumePushWC")
     val ssc = new StreamingContext(new SparkContext(conf), Seconds(5))
-    val flumeStream = FlumeUtils.createStream(ssc, "spark", 9888)
+    val flumeStream = FlumeUtils.createPollingStream(ssc, "spark", 9888)
     val wordcount = flumeStream.map(x => new String(x.event.getBody.array()).trim).flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_)
     wordcount.print()
 
